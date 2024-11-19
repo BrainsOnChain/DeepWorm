c40f994898223b35a35b284dbeec1066c184af92
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-19 13:52:12 +0530

    convert muscle outputs into movement assuming motor-like behaviour

--- nematoduino/src/main.cpp
+++ nematoduino/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <unistd.h>
 
@@ -6,13 +7,28 @@
 int main() {
   Worm worm;
 
+  int pos_x = 0;
+  int pos_y = 0;
+
+  int direction = 0;
+
   while (true) {
     worm.chemotaxis();
 
     std::cout << "Left: " << worm.getLeftMuscle()
               << ", Right: " << worm.getRightMuscle() << std::endl;
 
-    usleep(100000);
+    direction += (worm.getRightMuscle() - worm.getLeftMuscle()) / 5;
+    direction = direction % 360;
+    int distance = (worm.getRightMuscle() + worm.getLeftMuscle()) / 2;
+
+    pos_x += sin(direction * 2 * 3.14 / 360) * distance;
+    pos_y += cos(direction * 2 * 3.14 / 360) * distance;
+
+    std::cout << "D: " << direction << ", X: " << pos_x << ", Y: " << pos_y
+              << std::endl;
+
+    usleep(10000);
   }
 
   return 0;
