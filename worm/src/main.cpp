afe20b715626d9a50dfb1a58dc294511602fe78a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-22 14:43:38 +0530

    fix movement math, mainly turning direction was reversed

--- worm/src/main.cpp
+++ worm/src/main.cpp
@@ -18,12 +18,12 @@ int main() {
     std::cout << "Left: " << worm.getLeftMuscle()
               << ", Right: " << worm.getRightMuscle() << std::endl;
 
-    direction += (worm.getRightMuscle() - worm.getLeftMuscle()) / 5;
-    direction = direction % 360;
+    direction += -(worm.getRightMuscle() - worm.getLeftMuscle()) / 5;
+    direction = (direction + 360) % 360;
     int distance = (worm.getRightMuscle() + worm.getLeftMuscle()) / 2;
 
-    pos_x += sin(direction * 2 * 3.14 / 360) * distance;
-    pos_y += cos(direction * 2 * 3.14 / 360) * distance;
+    pos_x += sin(direction * 3.14 / 180) * distance;
+    pos_y += cos(direction * 3.14 / 180) * distance;
 
     std::cout << "D: " << direction << ", X: " << pos_x << ", Y: " << pos_y
               << std::endl;
