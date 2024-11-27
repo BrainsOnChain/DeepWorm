0c11b79eb84a9b3befde791d79504cc03078d012
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-27 14:24:22 +0530

    reimpl C main in rust

--- worm/src/main.rs
+++ worm/src/main.rs
@@ -1,4 +1,6 @@
 use std::ffi::{c_int, c_void};
+use std::thread::sleep;
+use std::time::Duration;
 
 #[link(name = "nematoduino")]
 extern "C" {
@@ -11,5 +13,33 @@ extern "C" {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let worm = unsafe { Worm_Worm() };
+
+    let mut pos_x = 0.0;
+    let mut pos_y = 0.0;
+
+    let mut direction = 0.0;
+
+    loop {
+        unsafe { Worm_chemotaxis(worm) };
+
+        let left = unsafe { Worm_getLeftMuscle(worm) };
+        let right = unsafe { Worm_getRightMuscle(worm) };
+
+        println!("Left: {}, Right: {}", left, right);
+
+        direction += -(right - left) as f64 / 5.0;
+        direction = direction.rem_euclid(360.0);
+        let distance = (right + left) as f64 / 100.0;
+
+        let new_pos_x = pos_x + (direction * 3.14 / 180.0).sin() * distance;
+        let new_pos_y = pos_y + (direction * 3.14 / 180.0).cos() * distance;
+
+        pos_x = new_pos_x;
+        pos_y = new_pos_y;
+
+        println!("D: {}, X: {}, Y: {}", direction, pos_x, pos_y);
+
+        sleep(Duration::from_millis(16));
+    }
 }
